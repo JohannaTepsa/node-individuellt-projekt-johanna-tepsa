d9b5e68dc4431713ab9d4151cc055f6f285be3fc
JohannaTepsa <johanna.tepsa@gmail.com>
2024-06-23 21:01:49 +0200

    Delete models/userModel.js

--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const userSchema = Joi.object({
-	username: Joi.string().alphanum().min(3).max(30).required(),
-	password: Joi.string().alphanum().min(6).max(30).required(),
-	email: Joi.string().email()
-});
-
-export default userSchema;
